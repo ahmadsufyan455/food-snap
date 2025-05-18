3fe4b669b45bd2f1263d7d500af6562bc27e2d6f
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-05-18 12:00:31 +0700

    feat: add generative ai package & setup env model

---
+++ lib/dtos/nutrition_dto.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class NutritionDto {
+  final double calories;
+  final double carbohydrates;
+  final double fat;
+  final double fiber;
+  final double protein;
+
+  NutritionDto({
+    required this.calories,
+    required this.carbohydrates,
+    required this.fat,
+    required this.fiber,
+    required this.protein,
+  });
+
+  factory NutritionDto.fromMap(Map<String, dynamic> map) {
+    return NutritionDto(
+      calories: (map['calories'] ?? 0).toDouble(),
+      carbohydrates: (map['carbohydrates'] ?? 0).toDouble(),
+      fat: (map['fat'] ?? 0).toDouble(),
+      fiber: (map['fiber'] ?? 0).toDouble(),
+      protein: (map['protein'] ?? 0).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'calories': calories,
+      'carbohydrates': carbohydrates,
+      'fat': fat,
+      'fiber': fiber,
+      'protein': protein,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NutritionDto.fromJson(String source) =>
+      NutritionDto.fromMap(json.decode(source) as Map<String, dynamic>);
+}
